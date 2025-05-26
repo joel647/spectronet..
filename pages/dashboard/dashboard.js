2a933587453b012edee9c141eb57f109ac616f95
joel647 <155640893+joel647@users.noreply.github.com>
2025-05-26 16:29:12 -0700

    Add files via upload

---
+++ pages/dashboard/dashboard.js
@@ -0,0 +1,25 @@
+
+
+
+
+function notAvailable(){
+    alert(clicked)
+    document.getElementById('showNotAvailable').style.display='block';
+}   
+
+
+let  user =  {
+    firstName:'joel',
+    lastName:'adeagbo',
+}
+
+const welcomeMessage = `Welcome back, ${user.firstName} ${user.lastName}!`;
+
+
+
+document.getElementById('welcomeMessage').textContent = welcomeMessage;
+console.log(welcomeMessage)
+
+let eye = document.getElementById('eye');
+console.log(eye)
+
---
+++ pages/faq/faq.js
@@ -0,0 +1,9 @@
+let paragraph = document.getElementById('paragraph');
+let btn = document.getElementById('btn');
+
+btn.addEventListener('onmouceover' ,'function');{
+    // paragraph.style.backgroundColor='red';
+    alert('clced')
+}
+
+
